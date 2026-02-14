7e8ad6fbdd6b963027797cfddaa437c66608d2a0
Aniket Dey <aniketdey2004@gmail.com>
2026-02-14 19:13:06 +0530

    image uploads using cloudinary done and nextconfig updated to accept images from cloudinary

--- app/api/event/route.ts
+++ app/api/event/route.ts
@@ -1,6 +1,7 @@
 import connectDB from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { Event } from "@/database";
+import cloudinary from "@/lib/cloudinaryConfig";
 export async function POST(req: NextRequest){
     try {
         await connectDB();
@@ -14,7 +15,26 @@ export async function POST(req: NextRequest){
         } catch (error) {
             return NextResponse.json({message: 'Invalid JSON data format'}, { status:400 })
         }
+        const file=formData.get('image') as File;
 
+        if(!file){
+            return NextResponse.json({message: 'Image file is required'}, {status:400});
+        }
+
+        const bytes=await file.arrayBuffer();
+        const buffer=Buffer.from(bytes);
+
+        const result=await new Promise((resolve,reject)=>{
+            cloudinary.uploader.upload_stream(
+                {folder: "nextjs_uploads"},
+                (error,result)=>{
+                    if(error) reject(error);
+                    else resolve(result);
+                }
+            ).end(buffer)
+        });
+
+        event.image=(result as {secure_url: string}).secure_url;
         const createdEvent=await Event.create(event);
 
         return NextResponse.json({ message: 'Event created successfully', event:createdEvent }, { status:201 });
