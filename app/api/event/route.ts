7f1d527116a73d7897b2587c5b7595f2e1df84c2
Aniket Dey <aniketdey2004@gmail.com>
2026-02-14 17:59:03 +0530

    Event creation route added

---
+++ app/api/event/route.ts
@@ -0,0 +1,25 @@
+import connectDB from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+import { Event } from "@/database";
+export async function POST(req: NextRequest){
+    try {
+        await connectDB();
+
+        const formData=await req.formData();
+
+        let event;
+
+        try {
+            event=Object.fromEntries(formData.entries());
+        } catch (error) {
+            return NextResponse.json({message: 'Invalid JSON data format'}, { status:400 })
+        }
+
+        const createdEvent=await Event.create(event);
+
+        return NextResponse.json({ message: 'Event created successfully', event:createdEvent }, { status:201 });
+    } catch (e) {
+        console.error(e);
+        return NextResponse.json({ message: 'Event Creation failed' , error: e instanceof Error ? e.message:'Unknown'}, { status:500 });
+    }
+}
